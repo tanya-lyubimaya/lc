fb5f24a27f95217df171c68873458a2197b8279b
tanya-lyubimaya <tatyana.lyubimaya.work@gmail.com>
2022-12-06 17:35:06 +0300

    🎨 Improved solution for 1480. Running Sum of 1d Array

--- 1480. Running Sum of 1d Array.go
+++ 1480. Running Sum of 1d Array.go
@@ -2,13 +2,11 @@ package lc
 
 func runningSum(nums []int) []int {
 	sums := make([]int, len(nums))
-
 	sums[0] = nums[0]
-	temp := nums[0]
 
 	for i := 1; i < len(nums); i++ {
-		temp += nums[i]
-		sums[i] = temp
+		sums[i] = nums[i] + sums[i-1]
 	}
+
 	return sums
 }
