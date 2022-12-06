e46ecae24400116a0f73a4e5a895c2e013b6c94e
tanya-lyubimaya <tatyana.lyubimaya.work@gmail.com>
2022-12-06 16:08:59 +0300

    🎨 Improved solution for 226. Invert Binary Tree

---
+++ 226. Invert Binary Tree.go
@@ -0,0 +1,41 @@
+package lc
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+
+	return root
+}
+
+//TODO: Add iterative solution
+/*
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        queue = collections.deque([root])
+        while queue:
+            current = queue.popleft()
+            current.left, current.right = current.right, current.left
+
+            if current.left:
+                queue.append(current.left)
+
+            if current.right:
+                queue.append(current.right)
+
+        return root
+*/
