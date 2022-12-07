e775b976a1a0830c9b924567bc95699cbdd98a50
tanya-lyubimaya <tatyana.lyubimaya.work@gmail.com>
2022-12-07 13:46:21 +0300

    ⚡ Optimised solution for 383. Ransom Note

--- 383. Ransom Note.go
+++ 383. Ransom Note.go
@@ -1,13 +1,20 @@
 package lc
 
 func canConstruct(ransomNote string, magazine string) bool {
+	if len(ransomNote) == 0 && len(magazine) == 0 {
+		return true
+	}
+	if len(ransomNote) > len(magazine) {
+		return false
+	}
+
 	letters := make(map[string]int)
 
-	for i := 0; i < len([]rune(magazine)); i++ {
+	for i := 0; i < len(magazine); i++ {
 		letters[string(magazine[i])]++
 	}
 
-	for i := 0; i < len([]rune(ransomNote)); i++ {
+	for i := 0; i < len(ransomNote); i++ {
 		// if a letter not in map or the counter == 0
 		if _, ok := letters[string(ransomNote[i])]; !ok || letters[string(ransomNote[i])] == 0 {
 			return false
