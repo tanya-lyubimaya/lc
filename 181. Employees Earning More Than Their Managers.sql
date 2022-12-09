28132343c9ab59559f3a91f0f7f924f824b7902b
tanya-lyubimaya <tatyana.lyubimaya.work@gmail.com>
2022-12-09 23:02:20 +0300

    ✨ Added 2 solutions for 181. Employees Earning More Than Their Managers

---
+++ 181. Employees Earning More Than Their Managers.sql
@@ -0,0 +1,2 @@
+SELECT e1.name AS Employee FROM Employee e1 LEFT JOIN Employee e2 ON e1.managerId = e2.id WHERE e1.salary > e2.salary;
+SELECT e1.name AS Employee FROM Employee e1, Employee e2 WHERE e1.managerId = e2.id AND e1.salary > e2.salary;
