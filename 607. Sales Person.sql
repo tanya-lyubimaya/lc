bea2ef63795590021a5d22bcc9f9af2ad7803d76
tanya-lyubimaya <tatyana.lyubimaya.work@gmail.com>
2022-12-14 18:16:14 +0300

    ✨ Added 2 solutions for 607. Sales Person

---
+++ 607. Sales Person.sql
@@ -0,0 +1,2 @@
+SELECT name FROM SalesPerson WHERE sales_id NOT IN (SELECT sales_id FROM Orders WHERE com_id IN (SELECT com_id FROM Company WHERE name = "RED"));
+SELECT name FROM SalesPerson WHERE sales_id NOT IN (SELECT sales_id FROM Orders o LEFT JOIN Company c ON o.com_id = c.com_id WHERE c.name = "RED");
