1340d307f8be919e3d9c5be0e137a15898d4a31a
tanya-lyubimaya <tatyana.lyubimaya.work@gmail.com>
2022-12-30 19:25:27 +0300

    ✨ Added 2 solutions for 414. Third Maximum Number

---
+++ 414. Third Maximum Number.go
@@ -0,0 +1,58 @@
+package lc
+
+import (
+	"math"
+	"sort"
+)
+
+func thirdMaxSorting(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(nums)))
+	elemCounted := 1
+	prevElem := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != prevElem {
+			elemCounted++
+			prevElem = nums[i]
+		}
+		if elemCounted == 3 {
+			return nums[i]
+		}
+	}
+	return nums[0]
+}
+
+func thirdMaxPointers(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	maxs := [3]int{math.MinInt, math.MinInt, math.MinInt}
+
+	for _, v := range nums {
+		if maxs[0] == v || maxs[1] == v || maxs[2] == v {
+			continue
+		}
+		if v > maxs[0] {
+			maxs[2] = maxs[1]
+			maxs[1] = maxs[0]
+			maxs[0] = v
+		} else if v > maxs[1] {
+			maxs[2] = maxs[1]
+			maxs[1] = v
+		} else if v > maxs[2] {
+			maxs[2] = v
+		}
+	}
+	if maxs[2] == math.MinInt {
+		return maxs[0]
+	} else {
+		return maxs[2]
+	}
+}
+
+// TODO: Approach 2: Min Heap Data Structure
+// TODO: Approach 3: Ordered Set
+// TODO: Approach 5: 3 Pointers (w/ booleans)
