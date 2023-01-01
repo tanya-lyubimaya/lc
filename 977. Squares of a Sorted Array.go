0fced7448323b8f9944759a961d573d2ce35cc4c
tanya-lyubimaya <tatyana.lyubimaya.work@gmail.com>
2023-01-01 20:55:00 +0300

    ✨ Added 1 more solution for 977. Squares of a Sorted Array

--- 977. Squares of a Sorted Array.go
+++ 977. Squares of a Sorted Array.go
@@ -5,11 +5,19 @@ import (
 	"sort"
 )
 
-func sortedSquares(nums []int) []int {
+func sortedSquaresFirstAttempt(nums []int) []int {
 	var squares []int
 	for _, v := range nums {
 		squares = append(squares, int(math.Pow(float64(v), 2)))
 	}
 	sort.Ints(squares)
 	return squares
+}
+
+func sortedSquares(nums []int) []int {
+	for i, v := range nums {
+		nums[i] = int(math.Pow(float64(v), 2))
+	}
+	sort.Ints(nums)
+	return nums
 }
