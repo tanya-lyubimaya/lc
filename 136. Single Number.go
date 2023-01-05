157b69aa95d77a9f0d41f95e61385f286720ee95
tanya-lyubimaya <tatyana.lyubimaya.work@gmail.com>
2023-01-05 20:54:18 +0300

    ✨ Added 2 more solutions for 136. Single Number

--- 136. Single Number.go
+++ 136. Single Number.go
@@ -32,3 +32,26 @@ func singleNumberSort(nums []int) int {
 	}
 	return nums[len(nums)-1]
 }
+
+func singleNumberSum(nums []int) int {
+	numsMap := make(map[int]int)
+	var numsSum, numsMapSum int
+	for _, v := range nums {
+		numsSum += v
+		if _, ok := numsMap[v]; !ok {
+			numsMap[v]++
+		}
+	}
+	for k, _ := range numsMap {
+		numsMapSum += k
+	}
+	return numsMapSum*2 - numsSum
+}
+
+func singleNumber(nums []int) int {
+	var xor int
+	for _, v := range nums {
+		xor ^= v
+	}
+	return xor
+}
