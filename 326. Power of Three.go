d80b6e4a5ba38248fada29fa012c0fa22a4a57f5
tanya-lyubimaya <tatyana.lyubimaya.work@gmail.com>
2023-01-11 21:22:40 +0300

    ✨ Added 2 more solutions for 326. Power of Three

--- 326. Power of Three.go
+++ 326. Power of Three.go
@@ -1,8 +1,13 @@
 package lc
 
-import "strconv"
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
 
-func isPowerOfThree(n int) bool {
+func isPowerOfThreeNaive(n int) bool {
 	res := 1
 	for res < n {
 		res *= 3
@@ -36,6 +41,16 @@ func isPowerOfThreeString(n int) bool {
 	return true
 }
 
-// TODO: Approach 3: Mathematics
+func isPowerOfThreeMathematics(n int) bool {
+	val := math.Log10(float64(n)) / math.Log10(3)
+	s := fmt.Sprintf("%v", val)
+	split := strings.Split(s, ".")
+	intPart, _ := strconv.ParseInt(split[0], 10, 0)
+	return int(math.Pow(3, float64(intPart))) == n
+}
+
+func isPowerOfThree(n int) bool {
+	return n > 0 && 1162261467%n == 0
+}
+
 // TODO: Solve with recursion
-// TODO: Approach 4: Integer Limitations
