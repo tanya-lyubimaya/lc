f0167bb7967966f98b44ae1c162ece1bae20c49e
tanya-lyubimaya <tatyana.lyubimaya.work@gmail.com>
2023-01-13 19:09:49 +0300

    ✨ Added 1 more solution for 268. Missing Number

--- 268. Missing Number.go
+++ 268. Missing Number.go
@@ -2,10 +2,27 @@ package lc
 
 import "sort"
 
-func missingNumber(nums []int) int {
+func missingNumberSort(nums []int) int {
 	sort.Ints(nums)
 	for i, v := range nums {
 		if i != v {
+			return i
+		}
+	}
+	return len(nums)
+}
+
+func missingNumber(nums []int) int {
+	type void struct{}
+	var member void
+	m := make(map[int]void)
+	for _, v := range nums {
+		if _, ok := m[v]; !ok {
+			m[v] = member
+		}
+	}
+	for i := 0; i < len(nums); i++ {
+		if _, ok := m[i]; !ok {
 			return i
 		}
 	}
