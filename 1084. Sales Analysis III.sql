bcbd3c55dd4a1f7ba4753aecd23106d059d9efc9
tanya-lyubimaya <tatyana.lyubimaya.work@gmail.com>
2023-01-15 00:07:02 +0300

    ✨ Added 1 more solution for 1084. Sales Analysis III

--- 1084. Sales Analysis III.sql
+++ 1084. Sales Analysis III.sql
@@ -1,3 +1,4 @@
 SELECT product_id, product_name FROM Product WHERE product_id IN (SELECT product_id FROM Sales WHERE sale_date >= '2019-01-01' AND sale_date <= '2019-03-31') AND product_id NOT IN (SELECT product_id FROM Sales WHERE sale_date < '2019-01-01' OR sale_date > '2019-03-31');
 SELECT product_id, product_name FROM Sales JOIN Product USING(product_id) GROUP BY product_id HAVING MIN(sale_date) >= '2019-01-01' AND MAX(sale_date) <= '2019-03-31';
 SELECT product_id, product_name FROM Product WHERE product_id IN (SELECT product_id FROM Sales GROUP BY product_id HAVING MIN(sale_date) >= '2019-01-01' AND MAX(sale_date) <= '2019-03-31');
+SELECT p.product_id, p.product_name FROM Product p LEFT JOIN Sales s USING(product_id) GROUP BY p.product_id HAVING MIN(s.sale_date) >= '2019-01-01' AND MAX(s.sale_date) <= '2019-03-31';
