98d5f763f69c7f8525b194a5e4e6047076a92e79
tanya-lyubimaya <tatyana.lyubimaya.work@gmail.com>
2023-01-16 23:26:14 +0300

    ✨ Added 1 more solution for 1148. Article Views I

--- 1148. Article Views I.sql
+++ 1148. Article Views I.sql
@@ -1,2 +1,3 @@
 SELECT DISTINCT author_id AS id FROM Views WHERE author_id = viewer_id ORDER BY id;
 SELECT author_id AS id FROM Views WHERE author_id = viewer_id GROUP BY id ORDER BY id;
+SELECT author_id AS id FROM Views GROUP BY id HAVING SUM(CASE WHEN author_id = viewer_id THEN 1 ELSE 0 END) > 0 ORDER BY id;
