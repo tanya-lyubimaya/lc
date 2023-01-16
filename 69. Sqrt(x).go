46ee1fd907436fd03aa641e00c484dccc1cab4a9
tanya-lyubimaya <tatyana.lyubimaya.work@gmail.com>
2023-01-16 23:14:30 +0300

    ✨ Added 1 more solution for 69. Sqrt(x)

--- 69. Sqrt(x).go
+++ 69. Sqrt(x).go
@@ -1,5 +1,7 @@
 package lc
 
+import "math"
+
 func mySqrt(x int) int {
 	i := 1
 	for i*i <= x {
@@ -7,3 +9,21 @@ func mySqrt(x int) int {
 	}
 	return i - 1
 }
+
+func mySqrtNewtonMethod(x int) int {
+	if x == 0 {
+		return 0
+	}
+	if x == 1 {
+		return 1
+	}
+	xPrev, xCurr := float64(x), float64(x/2)
+	precision := 0.1
+	for math.Abs(xPrev-xCurr) > precision {
+		xPrev = xCurr
+		xCurr = 0.5 * (xCurr + float64(x)/xCurr)
+	}
+	return int(xCurr)
+}
+
+// TODO: Binary search
