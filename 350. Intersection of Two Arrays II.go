254533d2e378c4abfda131982c5c296fb9f266b0
tanya-lyubimaya <tatyana.lyubimaya.work@gmail.com>
2023-01-18 17:02:58 +0300

    ✨ Added 1 more solution for 350. Intersection of Two Arrays II

--- 350. Intersection of Two Arrays II.go
+++ 350. Intersection of Two Arrays II.go
@@ -1,6 +1,8 @@
 package lc
 
-func intersect(nums1 []int, nums2 []int) []int {
+import "sort"
+
+func intersectHashMap(nums1 []int, nums2 []int) []int {
 	if len(nums1) == 1 && len(nums2) == 1 && nums1[0] == nums2[0] {
 		return []int{nums1[0]}
 	}
@@ -24,6 +26,28 @@ func intersect(nums1 []int, nums2 []int) []int {
 					result = append(result, k)
 				}
 			}
+		}
+	}
+	return result
+}
+
+func intersect(nums1 []int, nums2 []int) []int {
+	if len(nums1) == 1 && len(nums2) == 1 && nums1[0] == nums2[0] {
+		return []int{nums1[0]}
+	}
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	var i, j int
+	var result []int
+	for i < len(nums1) && j < len(nums2) {
+		if nums1[i] == nums2[j] {
+			result = append(result, nums1[i])
+			i++
+			j++
+		} else if nums1[i] < nums2[j] {
+			i++
+		} else {
+			j++
 		}
 	}
 	return result
