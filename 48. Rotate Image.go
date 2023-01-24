83fa522baaec5dcb53532af3e6960d383f61f49d
Tatyana Lyubimaya <tatyana.lyubimaya.work@gmail.com>
2023-01-24 17:47:45 +0300

    ✨ Added 1 more solution for 48. Rotate Image

--- 48. Rotate Image.go
+++ 48. Rotate Image.go
@@ -21,3 +21,25 @@ func rotateWithMap(matrix [][]int) {
 		j1--
 	}
 }
+
+func rotateWithNewMatrix(matrix [][]int) {
+	oldMatrix := make([][]int, len(matrix))
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[0]); j++ {
+			oldMatrix[i] = append(oldMatrix[i], matrix[i][j])
+		}
+	}
+	i := 0
+	j1 := len(matrix[0]) - 1
+	for i < len(matrix) && j1 >= 0 {
+		j := 0
+		i1 := 0
+		for j < len(matrix[0]) && i1 < len(matrix) {
+			matrix[i1][j1] = oldMatrix[i][j]
+			j++
+			i1++
+		}
+		i++
+		j1--
+	}
+}
