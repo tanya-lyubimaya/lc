dc97a1459f1385c0d4389a436ca5c35df5dfdce8
Tatyana Lyubimaya <tatyana.lyubimaya.work@gmail.com>
2023-01-24 17:52:19 +0300

    ✨ Added 1 more solution for 48. Rotate Image

--- 48. Rotate Image.go
+++ 48. Rotate Image.go
@@ -43,3 +43,16 @@ func rotateWithNewMatrix(matrix [][]int) {
 		j1--
 	}
 }
+
+func rotateGroups(matrix [][]int) {
+	n := len(matrix[0])
+	for i := 0; i < (n+1)/2; i++ {
+		for j := 0; j < n/2; j++ {
+			temp := matrix[n-1-j][i]
+			matrix[n-1-j][i] = matrix[n-1-i][n-j-1]
+			matrix[n-1-i][n-j-1] = matrix[j][n-1-i]
+			matrix[j][n-1-i] = matrix[i][j]
+			matrix[i][j] = temp
+		}
+	}
+}
