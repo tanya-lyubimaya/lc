954f3d8ce5dbda8aa598fce116a4ac654c7f8852
Tatyana Lyubimaya <tatyana.lyubimaya.work@gmail.com>
2023-01-24 18:03:30 +0300

    ✨ Added 1 more solution for 121. Best Time to Buy and Sell Stock

--- 121. Best Time to Buy and Sell Stock.go
+++ 121. Best Time to Buy and Sell Stock.go
@@ -1,6 +1,8 @@
 package lc
 
-func maxProfit(prices []int) int {
+import "math"
+
+func maxProfitBruteForce(prices []int) int {
 	var profit int
 	for i := 0; i < len(prices)-1; i++ {
 		for j := i + 1; j < len(prices); j++ {
@@ -10,4 +12,16 @@ func maxProfit(prices []int) int {
 		}
 	}
 	return profit
+}
+
+func maxProfit(prices []int) int {
+	minPrice, maxProfit := math.MaxInt, 0
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		} else if prices[i]-minPrice > maxProfit {
+			maxProfit = prices[i] - minPrice
+		}
+	}
+	return maxProfit
 }
