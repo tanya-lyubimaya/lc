3832f8c0720faef47ebcb8e98c38cfdbc307e2b6
Tatyana Lyubimaya <tatyana.lyubimaya.work@gmail.com>
2023-01-26 17:28:55 +0300

    ✨ Added 1 more solution for 122. Best Time to Buy and Sell Stock II

--- 122. Best Time to Buy and Sell Stock II.go
+++ 122. Best Time to Buy and Sell Stock II.go
@@ -13,3 +13,28 @@ func maxProfit(prices []int) int {
 	}
 	return maxprofit
 }
+
+func maxProfitBruteForce(prices []int) int {
+	res := calculate(prices, 0)
+	return res
+}
+
+func calculate(prices []int, s int) int {
+	if s >= len(prices) {
+		return 0
+	}
+	var max int
+	for i := s; i < len(prices); i++ {
+		var maxprofit int
+		for j := i + 1; j < len(prices); j++ {
+			profit := calculate(prices, j+1) + prices[j] - prices[i]
+			if profit > maxprofit {
+				maxprofit = profit
+			}
+		}
+		if maxprofit > max {
+			max = maxprofit
+		}
+	}
+	return max
+}
